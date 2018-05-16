c4b3a765a563602dcc122b79a7045d87ff264c94
Carl Hillerman <chillerm@gmail.com>
2018-05-16 00:25:29 -0400

    Renamed App.

---
+++ src/main/java/hillermann/carl/learning/baseapp/Main.java
@@ -0,0 +1,14 @@
+package hillermann.carl.learning.baseapp;
+
+public class Main {
+
+    public static void main(String... args) {
+        new Main().printAnswer();
+    }
+
+    public void printAnswer() {
+//        String output = Strings.concat(" The answer is    ", Numbers.add(19, 23));
+//        System.out.println(output);
+        System.out.println("printAnswer");
+    }
+}
